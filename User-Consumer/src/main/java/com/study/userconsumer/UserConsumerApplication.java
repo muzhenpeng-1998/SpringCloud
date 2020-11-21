b8c18514d31b3451ed5db77786e37f3d5bd264cb
muzhenpeng <zhiwei741@icloud.com>
2020-11-21 19:05:36 +0800

    熔断 降级

--- User-Consumer/src/main/java/com/study/userconsumer/UserConsumerApplication.java
+++ User-Consumer/src/main/java/com/study/userconsumer/UserConsumerApplication.java
@@ -2,17 +2,33 @@ package com.study.userconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author zhenpengmu
  */
 @SpringBootApplication
 @EnableFeignClients
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class UserConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserConsumerApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	RestTemplate getRestTemplate() {
+
+		RestTemplate restTemplate = new RestTemplate();
+		//	restTemplate.getInterceptors().add(new LoggingClientHttpRequestInterceptor());
+		return restTemplate;
 	}
 
 }
---
+++ User-Consumer/src/main/java/com/study/userconsumer/UserProviderBack.java
@@ -0,0 +1,56 @@
+package com.study.userconsumer;
+
+import com.study.UserAPI.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class UserProviderBack implements ConsumerApi {
+
+    @Override
+    public String alive() {
+
+
+
+        return "降级了";
+    }
+
+    @Override
+    public String getById(Integer id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Person postPserson(Person person) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Map<Integer, String> getMap(Integer id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Map<Integer, String> getMap2(Integer id, String name) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Map<Integer, String> getMap3(Map<String, Object> map) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Map<Integer, String> postMap(Map<String, Object> map) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
+
